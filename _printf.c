3ef1d1c474cab24666a8a9c38083200542ca8a0f
AissamDEV <aissamaboukassim@gmail.com>
2023-04-25 04:20:04 -0700

    corrected

--- _printf.c
+++ _printf.c
@@ -1,55 +1,5 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-/**
- * print_arg - Print the argument based on the specified conversion specifier.
- * @args: The list of arguments.
- * @specifier: The conversion specifier.
- *
- * Return: The number of characters printed.
- */
-int print_arg(va_list args, char specifier)
-{
-	int len = 0;
-
-	switch (specifier)
-	{
-		case 'c':
-			len += putchar(va_arg(args, int));
-			break;
-		case 's':
-			len += printf("%s", va_arg(args, char *));
-			break;
-		case 'd':
-		case 'i':
-			len += printf("%d", va_arg(args, int));
-			break;
-		case 'u':
-			len += printf("%u", va_arg(args, unsigned int));
-			break;
-		case 'o':
-			len += printf("%o", va_arg(args, unsigned int));
-			break;
-		case 'x':
-			len += printf("%x", va_arg(args, unsigned int));
-			break;
-		case 'X':
-			len += printf("%X", va_arg(args, unsigned int));
-			break;
-		case 'p':
-			len += printf("%p", va_arg(args, void *));
-			break;
-		case '%':
-			len += putchar('%');
-			break;
-		default:
-			len += putchar('%');
-			len += putchar(specifier);
-			break;
-	}
-	return (len);
-}
 /**
  * _printf - Print a formatted string to stdout.
  * @format: Format string.
@@ -58,24 +8,43 @@ int print_arg(va_list args, char specifier)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int len = 0;
+	int num_chars = 0;
+	char c;
+	va_list arg_list;
 
-	va_start(args, format);
+	va_start(arg_list, format);
 
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			len += print_arg(args, *format);
-		}
-		else
+			if (*format == 'c')
+			{
+				c = (char) va_arg(arg_list, int);
+				putchar(c);
+				num_chars++;
+			} else if (*format == 's')
+			{
+				char *s = va_arg(arg_list, char *);
+
+				while (*s != '\0')
+				{
+					putchar(*s);
+					s++;
+					num_chars++;
+				}
+			} else if (*format == '%')
+			{
+				putchar('%');
+				num_chars++;
+			}
+		} else
 		{
-			len += putchar(*format);
+			putchar(*format);
+			num_chars++;
 		}
 		format++;
-	}
-	va_end(args);
-	return (len);
+	} va_end(arg_list);
+	return (num_chars);
 }
