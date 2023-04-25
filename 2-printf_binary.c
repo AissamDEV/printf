8b427af3d554ac8f8cca1f123243072a37c7bc97
AissamDEV <aissamaboukassim@gmail.com>
2023-04-25 14:45:33 -0700

    correct

--- 2-printf_binary.c
+++ 2-printf_binary.c
@@ -1,16 +1,4 @@
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-/**
-  *_putchar - function that outputs a single characte
-  *@c: character
-  *Return: putchar
-  */
-int _putchar(char c)
-{
-	return (putchar(c));
-}
 /**
   *print_binary - function thats print a binary
   *@n: unsigned integer to print in binary
@@ -21,42 +9,4 @@ void print_binary(unsigned int n)
 	if (n > 1)
 		print_binary(n / 2);
 		_putchar((n % 2) + '0');
-}
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int printed = 0;
-	unsigned int n;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'b')
-			{
-				n = va_arg(args, unsigned int);
-				print_binary(n);
-				printed += sizeof(unsigned int) * 8;
-			}
-			else
-			{
-				_putchar('%');
-				printed++;
-				if (*format)
-					_putchar(*format);
-				printed++;
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (printed);
 }
