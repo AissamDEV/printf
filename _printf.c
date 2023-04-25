ed6a3415488d1df4c8bc396b67c60a3c86dd9b6c
AissamDEV <aissamaboukassim@gmail.com>
2023-04-25 07:23:11 -0700

    corrected

--- _printf.c
+++ _printf.c
@@ -3,19 +3,6 @@
 
 void print_buffer(char buffer[], int *buff_ind);
 /**
-  * print_buffer - Prints the contents of the buffer if it exist
-  * @buffer: Array of chars
-  * @buff_ind: Index at which to add next char, represents the length.
-  */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-	{
-		write(1, &buffer[0], *buff_ind);
-		*buff_ind = 0;
-	}
-}
-/**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
@@ -56,4 +43,19 @@ int _printf(const char *format, ...)
 	} print_buffer(buffer, &buff_ind);
 	va_end(list);
 	return (p_chars);
+}
+/**
+  * print_buffer - Prints the content
+  * of the buffer if it exist
+  * @buffer: Array of chars
+  * @buff_ind: Index at which to add
+  * next char, represents the length.
+  */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+	{
+		write(1, &buffer[0], *buff_ind);
+		*buff_ind = 0;
+	}
 }
