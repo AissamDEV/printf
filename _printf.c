9ab4aa7a0d38881aa90f92e209f90922c5c7a75e
AissamDEV <aissamaboukassim@gmail.com>
2023-04-25 07:31:15 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,61 +1,51 @@
-#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Print a formatted string to stdout.
+ * @format: Format string.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int i, p = 0, p_chars = 0, flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	int num_chars = 0;
+	char c;
+	va_list arg_list;
 
-	if (format == NULL)
-		return (-1);
+	va_start(arg_list, format);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	while (*format != '\0')
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			p_chars++;
+			format++;
+			if (*format == 'c')
+			{
+				c = (char) va_arg(arg_list, int);
+				putchar(c);
+				num_chars++;
+			} else if (*format == 's')
+			{
+				char *s = va_arg(arg_list, char *);
+
+				while (*s != '\0')
+				{
+					putchar(*s);
+					s++;
+					num_chars++;
+				}
+			} else if (*format == '%')
+			{
+				putchar('%');
+				num_chars++;
+			}
 		} else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			p = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (p == -1)
-				return (-1);
-			p_chars += p;
+			putchar(*format);
+			num_chars++;
 		}
-	} print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (p_chars);
-}
-/**
-  * print_buffer - Prints the content
-  * of the buffer if it exist
-  * @buffer: Array of chars
-  * @buff_ind: Index at which to add
-  * next char, represents the length.
-  */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-	{
-		write(1, &buffer[0], *buff_ind);
-		*buff_ind = 0;
-	}
+		format++;
+	} va_end(arg_list);
+	return (num_chars);
 }
