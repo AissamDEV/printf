d73d8428df78c55847874fa6c508e7d8b3586795
AissamDEV <aissamaboukassim@gmail.com>
2023-04-25 14:19:06 -0700

    changed way

--- 2-printf_binary.c
+++ 2-printf_binary.c
@@ -1,28 +1,23 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
+#include "_printf.c"
+#include <stdlib.h>
 /**
- * print_binary - converts an unsigned int argument
- *to binary and print it
- * @args: the argument to be converted and printed
- * @spec: the format specifier for the argument
- *
- * Return: the number of digits printed
- *
- */
-int print_binary(va_list args, format_specifier_t *spec)
+  *_putchar - function that outputs a single characte
+  *@c: character
+  *Return: putchar
+  */
+int _putchar(char c)
 {
-	unsigned int num = va_arg(args, unsigned int);
-	char binary_str[33];
-	int i = 0;
-
-	while (num)
-		binary_str[i++] = (num & 1) + '0';
-		num >>= 1;
-
-	binary_str[i] = '\0';
-	str_reverse(binary_str);
-
-	return (print_string(binary_str, spec));
+	return (putchar(c));
+}
+/**
+  *print_binary - function thats print a binary
+  *@n: unsigned integer to print in binary
+  *Return: void
+  */
+void print_binary(unsigned int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+		_putchar((n % 2) + '0');
 }
